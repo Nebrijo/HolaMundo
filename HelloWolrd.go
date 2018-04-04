3ba8add6cedf361db247d455f87b6a2e2e5bf2b4
Nebrijo <38077569+Nebrijo@users.noreply.github.com>
2018-04-04 09:10:03 -0600

    Create HelloWolrd.go

---
+++ HelloWolrd.go
@@ -0,0 +1,6 @@
+package main //Paquete principal donde se contiene el fmt
+import "fmt" //Es la función necesaria para mostrar mensajes en pantalla
+
+func main(){
+  fmt.Println("Hola mundo") //Sentencia para mostrar el mensaje
+}
